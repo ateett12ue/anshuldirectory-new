61416988edd04f0cd070c7261b64689fc95971a3
Test <test@Tests-MacBook-Air.local>
2025-05-13 17:10:15 +0530

    Updating Index

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export interface Person {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  state: string;
+  city: string;
+}
+
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  state: string;
+  city: string;
+} 
